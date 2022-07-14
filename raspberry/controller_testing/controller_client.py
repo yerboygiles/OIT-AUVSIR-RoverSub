cebbba85ba54326829dba7ec33ff329353204804
unknown <coltonrhodes24@gmail.com>
2022-07-14 16:04:08 -0700

    server deez nuts

---
+++ raspberry/controller_testing/controller_client.py
@@ -0,0 +1,18 @@
+import socket
+
+TCP_IP = '64.112.174.137' # this IP of my pc. When I want raspberry pi 2`s as a server, I replace it with its IP '169.254.54.195'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024 # Normally 1024, but I want fast response
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print ('Connection address:', addr)
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: break
+    print ("received data:", data)
+    conn.send(data)  # echo
+conn.close()
