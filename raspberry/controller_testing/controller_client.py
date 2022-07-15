3d0506b56397d216a5a3a1886c50de2390cd4faa
unknown <coltonrhodes24@gmail.com>
2022-07-15 14:08:54 -0700

    server penis

--- raspberry/controller_testing/controller_client.py
+++ raspberry/controller_testing/controller_client.py
@@ -1,6 +1,6 @@
 import socket
 
-TCP_IP = '192.168.137.31' # this IP of my pc. When I want raspberry pi 4`s as a server
+TCP_IP = '64.112.174.137' # this IP of my pc. When I want raspberry pi 4`s as a server use "192.168.137.31"
 TCP_PORT = 5005
 BUFFER_SIZE = 1024 # Normally 1024, but I want fast response
 
